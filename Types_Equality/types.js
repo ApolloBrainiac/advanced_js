96685de9b971b42eeec31d61bd1ed587c1b21a66
Andrew M. Perez <AxMPerez@gmail.com>
2017-07-25 17:09:29 -0400

    NaN lecture

--- Types_Equality/types.js
+++ Types_Equality/types.js
@@ -1,44 +1,16 @@
 "use strict";
 
-/* What are the different types in js?
+// What is the type of NaN?
 
-Boolean
-Number
-String
-Null
-Undefined
-
-Object
-
-Dynamive vs Static
-
-String a = 'moo';
-
-After a var is declared in a static language that
-variable will only hold the type of variable first
-declared
-
-*/
-
-
-
-// What is the difference between == (equality) and === (strict equality)?
-//  with == type cooersion
-0 === 0
-
-// true
-
-0 !== 1
-
-// true
-
-0 == 0
-// true
-
-'' == '0'
+// NaN == ANYTHING
 // false
 
-0 == ''
-// true
+isNaN(NaN);
 
-0 == '0' //true
+isNaN("1");
+
+isNaN("A");
+
+var a = NaN;
+a !== a;
+// true
