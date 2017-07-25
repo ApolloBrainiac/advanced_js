e324040658716ecfcf066e26b1f204869ebe2adf
Andrew M. Perez <AxMPerez@gmail.com>
2017-07-25 16:51:42 -0400

    typof lecture

--- Types_Equality/types.js
+++ Types_Equality/types.js
@@ -10,5 +10,15 @@ Undefined
 
 Object
 
+Dynamive vs Static
+
+String a = 'moo';
+
+After a var is declared in a static language that
+variable will only hold the type of variable first
+declared
+
 */
 
+var a;
+console.log(a);
