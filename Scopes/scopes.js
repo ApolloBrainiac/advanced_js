afa49a68fc96e51e2c0800fadb186c53dd5eb092
Andrew M. Perez <AxMPerez@gmail.com>
2017-08-02 14:42:55 -0400

    finish function closures lesson

--- Scopes/scopes.js
+++ Scopes/scopes.js
@@ -1,8 +1,58 @@
 "use strict";
 
-// What is an IIFE(iffy) Immediatly Invoked Feunction Expression
-// and why might you use it?
-(function(){
-	var thing = {'hello':'main'};
-	console.log("main: ", thing);
-})();
+// What are function closures?
+
+// function sayHello(name) {
+// 	var text = 'Hello ' + name;
+// 	console.log(text);
+// };
+
+// sayHello("Asim");
+
+// function sayHello(name) {
+// 	var text = 'Hello ' + name;
+// 	return function() {
+// 		console.log(text);
+// 	}
+// };
+
+// sayHello("Asim")();
+
+// function sayHello(name) {
+// 	var text = 'Hello ' + name;
+// 	return function() {
+// 		console.log(text);
+// 	}
+// };
+
+// var sayAsim = sayHello("Asim");
+// sayAsim();
+
+// When a function returns a function, the one returned
+// keeps any referencet to variables that it used from
+// the first function
+
+// var foo = [];
+// for (var i = 0; i < 10; i++) {
+// 	foo[i] = function() { return i };
+
+// }
+// console.log(foo[0]());
+// console.log(foo[1]());
+// console.log(foo[2]());
+
+	// in a closure, a var is not a copy
+	// but points to the actual value of the var
+	// in the outer scope
+	// "CURRENT VALUE"
+
+var foo = [];
+for (var i = 0; i < 10; i++) {
+	(function(){
+		var y = i;
+		foo[i] = function() { return y};
+	})();
+}
+console.log(foo[0]());
+console.log(foo[1]());
+console.log(foo[2]());
